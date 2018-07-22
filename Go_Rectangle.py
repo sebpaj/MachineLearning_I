ffd3bfc47d8e2e884b14be0b4656a14f8387ba9d
spyddy93 <sseba07@gmail.com>
2018-07-22 18:16:50 +0200

    initializing python game

---
+++ Go_Rectangle.py
@@ -0,0 +1,23 @@
+import pygame
+
+pygame.init()
+
+win = pygame.display.set_mode((500, 500))
+
+pygame.display.set_caption("Go Rectangle Go")
+
+x=50
+y=50
+width = 40
+height = 40
+velocity = 5
+
+run = True
+while run:
+    pygame.time.delay(100)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
