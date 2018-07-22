2f8c5e32e527594c6e7f607b7b8e992da48f7952
spyddy93 <sseba07@gmail.com>
2018-07-22 19:03:08 +0200

    adding basic movement and boundaries

--- Go_Rectangle.py
+++ Go_Rectangle.py
@@ -2,21 +2,40 @@ import pygame
 
 pygame.init()
 
-win = pygame.display.set_mode((500, 500))
+screenWidth = 800
+screenHeight = 600
+
+win = pygame.display.set_mode((screenWidth, screenHeight))
 
 pygame.display.set_caption("Go Rectangle Go")
 
 x=50
-y=50
-width = 40
-height = 40
-velocity = 5
+y=500
+width = 20
+height = 20
+velocity = 10
 run = True
+
 while run:
     pygame.time.delay(100)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT] and x > 0:
+        x -= velocity
+    if keys[pygame.K_RIGHT] and x < screenWidth-width:
+        x += velocity
+    if keys[pygame.K_UP] and y > 0:
+        y -= velocity
+    if keys[pygame.K_DOWN] and y < screenHeight-height:
+        y += velocity
+
+    win.fill((0,0,0))
+    pygame.draw.rect(win, (139, 157, 186), (x, y, width, height))
+    pygame.display.update()
 
 pygame.quit()
